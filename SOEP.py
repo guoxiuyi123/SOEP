25571f3e91ceb9ce8cc0c26e97e8e860f1e39bcb
guoxiuyi123 <129658173+guoxiuyi123@users.noreply.github.com>
2026-01-30 15:02:14 +0800

    Add files via upload
    main

---
+++ SOEP.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+
+
+class FGM(nn.Module):
+    def __init__(self, dim) -> None:
+        super().__init__()
+
+        self.conv = nn.Conv2d(dim, dim*2, 3, 1, 1, groups=dim)
+
+        self.dwconv1 = nn.Conv2d(dim, dim, 1, 1, groups=1)
+        self.dwconv2 = nn.Conv2d(dim, dim, 1, 1, groups=1)
+        self.alpha = nn.Parameter(torch.zeros(dim, 1, 1))
+        self.beta = nn.Parameter(torch.ones(dim, 1, 1))
+
+    def forward(self, x):
+        # res = x.clone()
+        fft_size = x.size()[2:]
+        x1 = self.dwconv1(x)
+        x2 = self.dwconv2(x)
+
+        x2_fft = torch.fft.fft2(x2, norm='backward')
+
+        out = x1 * x2_fft
+
+        out = torch.fft.ifft2(out, dim=(-2,-1), norm='backward')
+        out = torch.abs(out)
+
+        return out * self.alpha + x * self.beta
+
+class OmniKernel(nn.Module):
+    def __init__(self, dim) -> None:
+        super().__init__()
+
+        ker = 41
+        pad = ker // 2
+        self.in_conv = nn.Sequential(
+                    nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1),
+                    nn.GELU()
+                    )
+        self.out_conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1)
+        self.dw_13 = nn.Conv2d(dim, dim, kernel_size=(1,ker), padding=(0,pad), stride=1, groups=dim)
+        self.dw_31 = nn.Conv2d(dim, dim, kernel_size=(ker,1), padding=(pad,0), stride=1, groups=dim)
+        self.dw_33 = nn.Conv2d(dim, dim, kernel_size=ker, padding=pad, stride=1, groups=dim)
+        self.dw_11 = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=dim)
+
+        self.act = nn.ReLU()
+
+        ### sca ###
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.pool = nn.AdaptiveAvgPool2d((1,1))
+
+        ### fca ###
+        self.fac_conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.fac_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fgm = FGM(dim)
+
+    def forward(self, x):
+        out = self.in_conv(x)
+
+        ### fca ###
+        x_att = self.fac_conv(self.fac_pool(out))
+        x_fft = torch.fft.fft2(out, norm='backward')
+        x_fft = x_att * x_fft
+        x_fca = torch.fft.ifft2(x_fft, dim=(-2,-1), norm='backward')
+        x_fca = torch.abs(x_fca)
+
+        ### fca ###
+        ### sca ###
+        x_att = self.conv(self.pool(x_fca))
+        x_sca = x_att * x_fca
+        ### sca ###
+        x_sca = self.fgm(x_sca)
+
+        out = x + self.dw_13(out) + self.dw_31(out) + self.dw_33(out) + self.dw_11(out) + x_sca
+        out = self.act(out)
+        return self.out_conv(out)
+
+class CSPOmniKernel(nn.Module):
+    def __init__(self, dim, e=0.25):
+        super().__init__()
+        self.e = e
+        self.cv1 = Conv(dim, dim, 1)
+        self.cv2 = Conv(dim, dim, 1)
+        self.m = OmniKernel(int(dim * self.e))
+
+    def forward(self, x):
+        ok_branch, identity = torch.split(self.cv1(x), [int(x.size(1) * self.e), int(x.size(1) * (1 - self.e))], dim=1)
+        return self.cv2(torch.cat((self.m(ok_branch), identity), 1))
